136982218c770f6cf8357dfc6d60a57c565cec3d
AZO234 <typesylph@gmail.com>
2026-03-26 18:15:11 +0900

    1.0.2

--- src/App.vue
+++ src/App.vue
@@ -135,7 +135,7 @@ function onKeydown(e: KeyboardEvent) {
           <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">ポスト</a>
           <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2FAZO234.github.io%2Fjapanese-character-viewer&layout&size&width=93&height=20&appId" width="93" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
           <div class="line-it-button" data-lang="ja" data-type="share-a" data-env="REAL" data-url="https://azo234.github.io/japanese-character-viewer/" data-color="default" data-size="small" data-count="false" data-ver="3" style="display: none;"></div>
-          <a href="https://coff.ee/azo234" target="_blank" rel="noopener noreferrer">
+          <a href="https://buymeacoffee.com/azo234" target="_blank" rel="noopener noreferrer">
             <img src="https://img.shields.io/badge/-Buy%20Me%20a%20Coffee-ffdd00?style=flat-square&logo=buymeacoffee&logoColor=black" alt="Buy Me a Coffee">
           </a>
           <a href="https://github.com/sponsors/azo234" target="_blank" rel="noopener noreferrer">
